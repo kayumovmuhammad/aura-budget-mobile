3735119bf22ac313c87c3b17643dc12e50eb2a89
Muhammad Kayumov <kayumovmuhamma@gmail.com>
2026-03-16 21:39:47 +0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Aura Budget Mobile',
+  webDir: 'dist'
+};
+
+export default config;
